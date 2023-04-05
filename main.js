37d36f56eb41e49532df024dbcd1ecc6b1bd886d
Victorqqeeee <113634377+Victorqqeeee@users.noreply.github.com>
2023-04-05 16:53:27 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -37,4 +37,32 @@ gameStatus = "";
 
 function setup(){
 canvas =  createCanvas(700,550);
+canvas.parent('canvas');
+
+video = createCapture(VIDEO);
+video.size(700, 550);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose, gotPoses');
+}
+
+function modelLoaded() {
+  console.log(poseNet Is Initialized);
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+{
+
+  rightWristY = results[0].pose.rightWrist.y;
+  rightWristX = results[0].pose.rightWrist.y;
+  scoreRightWrist = results[0].pose.keypoints[10].score;
+  console.log(scoreRightWrist);
+  }
+}
+
+function startGame() {
+  gameStatus = "start";
 }
